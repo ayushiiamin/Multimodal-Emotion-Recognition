2a7bfec3beadba0705dce5ad9d26c422517c3652
Ayushi Amin <77382840+ayushiiamin@users.noreply.github.com>
2022-01-16 01:58:02 +0400

    Added the libraries

--- Dissertation/main.py
+++ Dissertation/main.py
@@ -1,3 +1,20 @@
 """ The main class """
 
-import 
+""" Importing the required libraries """
+
+#%%
+import sys
+assert sys.version_info >= (3, 5)
+
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+import numpy as np
+import os
+import tarfile
+import urllib
+import pandas as pd
+import urllib.request
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
